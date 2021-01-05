de64fd350fb2141623488c5e5178d103b5361a54
rifto221 <77001307+rifto221@users.noreply.github.com>
2021-01-05 11:06:52 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Agence de transport et logement pour étudiant</title>
+</head>
+<body>
+
+	<header>
+		<nav>
+			<ul>
+				<li id="logo"><a href="#">Clos Marsault</a></li>
+				<li><a href="#langue">Langue</a></li>
+				<li><a href="#prestation">Réserver</a></li>
+				<li><a href="#formulaire">Nous contacter</a></li>
+			</ul>
+		</nav>
+
+		<div id="imagesPrincipale">
+			<h1>Clos Marsault</h1>
+			<div id="premier-trait"></div>
+			<h3>Chambre d'hote</h3>
+		</div>
+	</header>
+
+	<section id="presentation">
+		<div id="text-introduction">
+			<h2>Un lieu unique pour un séjour unique</h2>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Minus dignissimos quas totam error, accusantium veniam ipsam pariatur ipsum beatae sed reiciendis optio sit possimus rem eaque magni alias consectetur recusandae impedit corrupti doloribus cupiditate? Repellat, consequatur, ipsam sit tempora eos vero quam esse aliquam. Error cum optio ad maiores soluta ut, eum expedita corporis tempore, consequuntur deserunt iure vero inventore, dolores modi reiciendis eaque quos a. Distinctio numquam expedita soluta, voluptas minus cum ut, laudantium magni eius? Est, nemo, explicabo.</p>
+		</div>
+
+		<div id="prestation">
+			<div class="image-prestation">
+				<h4>Nous trouver</h4>
+				<a href="#"><img src="img/carte.jpg" alt="Une carte en image"></a>
+			</div>
+				
+			<div class="image-prestation">
+				<h4>Coté chambre</h4>
+				<a href="#"><img src="img/chambre.jpg" alt="Une chambre en image"></a>
+			</div>
+
+			<div class="image-prestation">
+				<h4>Coté cuisine</h4>
+				<a href="#"><img src="img/repas.jpg" alt="Un repas en image"></a>
+			</div>
+		</div>
+
+	
+	</section>
+
+	<section id="tourisme">
+		<h2>Et tant à découvrir....</h2>
+		<ul>
+			<li id="ocean"><p>L'océan Atlantique</p></li>
+			<li id="vignoble"><p>Le vignoble Bordelais</p></li>
+			<li id="chateau"><p>L'estuaire de Gironde</p></li>
+			<li id="phare"><p>Les phare de Médoc</p></li>
+		</ul>
+		
+	</section>
+
+	<footer>
+		<h2  id="formulaire">Contacter nous</h2>
+		<form>
+			<input type="text" name="" placeholder="nom">
+			<input type="text" name="" placeholder="e-mail">
+			<textarea placeholder="votre message..."></textarea>
+			<button>envoyer</button>
+			 
+		</form>
+
+		<div id="deuxieme-trait"></div>
+		<div id="copyrightEtIcons">
+			<div id="copyright">
+				<span>&copy; fromRifto(); 2021</span>
+				
+			</div>
+
+			<div id="icons">
+				<a href="http://www.twitter.fr"><i class="fa fa-twitter"></i></a>
+
+				<a href="http://www.facebook.fr"><i class="fa fa-facebook"></i></a>
+							
+				<a href="http://www.instagram.com"><i class="fa fa-instagram"></i></a>
+			</div>
+		</div>
+	</footer>
+	
+</body>
+</html>
